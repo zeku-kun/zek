0501e39cb439d90de56dbcd67ec06c4cad13698a
Zeku <zeku.kun15@gmail.com>
2026-07-20 16:29:27 +0800

    Version 1.2 --Added Game class(outer class) and Mechanics class(inner class)--

--- experiment02/experiment01.cpp
+++ experiment02/experiment01.cpp
@@ -2,16 +2,37 @@
 #include <raylib.h>
 #include <cmath>
 using namespace std;
+class Game{
+    public:
+    double deltaTime;
+    static inline const int SCREEN_WIDTH = 700;
+    static inline const int SCREEN_HEIGHT = 600;
+    const char GAME_TITLE[14] = "RANDOM THINGS";
 
-const int SCREEN_WIDTH = 700;
-const int SCREEN_HEIGHT = 600;
-const char GAME_TITLE[] = "RANDOM THINGS";
-int gravMult = 1;
-const float gravity = 9.81;
-double deltaTime;
+    protected:
+    static inline int gravMult = 2;
+    static inline const float gravity = 9.81;
+    
+    class Mechanics{
+        protected:
+        Vector2 velocity = {0, 0};
+
+        void Grav(float &pos_y, Game* g){
+            if(pos_y <= SCREEN_HEIGHT){
+                velocity.y += (gravity * gravMult) * g -> deltaTime;
+                pos_y += velocity.y;
+            }
+
+            if(pos_y >= SCREEN_HEIGHT){
+                velocity.y = 0;
+            }
+        }
+    };
+};
 
 
-class Ball{
+
+class Ball: public Game, protected Game::Mechanics{
     protected:
     static int objCount;
 
@@ -19,8 +40,10 @@ class Ball{
     Vector2 pos = {0 , 0};
     int radius;
     Color color;
-    float velocityY = 0;
 
+    struct Walking{
+        
+    };
 
     Ball(){
         objCount++;
@@ -35,8 +58,9 @@ class Ball{
             pos.x += -5;
         }
         if(IsKeyDown(KEY_SPACE) && pos.y >= SCREEN_HEIGHT - radius){
-            velocityY = -9.8;
-            pos.y += velocityY;
+            velocity.y = -8;
+            pos.y += velocity.y;
+            
         }
     }
 
@@ -52,13 +76,11 @@ class Ball{
     }
 
     void Gravity(){
-        if(pos.y <= SCREEN_HEIGHT){
-            velocityY += (gravity * gravMult) * (deltaTime);
-            pos.y += velocityY;
+        Grav(pos.y, this);
+
+        if(pos.y >= SCREEN_HEIGHT - radius){
+            velocity.y = 0;
         }
-        /*if(pos.y >= SCREEN_HEIGHT - radius){
-            velocityY = 0;
-        }*/
     }
 
     void Update(){
@@ -68,25 +90,39 @@ class Ball{
     }
 };
 
+class Box{
+    public:
+    Rectangle rec;
+    Vector2 origin;
+    float rotation;
+    Color color;
+
+    void Draw(){
+        DrawRectanglePro(rec, origin, rotation, color);
+    }
+};
+
 int Ball::objCount = 0 ;
 Ball ball;
+Game game;
 
 int main(){
     
 
     // Ball
-    ball.pos = {SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2};
+    ball.pos = {static_cast<float>(game.SCREEN_WIDTH) / 2, static_cast<float>(game.SCREEN_HEIGHT) / 2};
     ball.radius = 20;
     ball.color = BLACK;
 
     SetTraceLogLevel(LOG_ERROR);
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, GAME_TITLE);
+    InitWindow(game.SCREEN_WIDTH, game.SCREEN_HEIGHT, game.GAME_TITLE);
     SetTargetFPS(60);
 
     while(!WindowShouldClose()){
-        deltaTime = GetFrameTime();
-        cout << ball.velocityY << ":"<< ball.pos.y << "\n";
+        ball.deltaTime = GetFrameTime();
+        
         BeginDrawing();
+        
         // Update
         ball.Update();
 
